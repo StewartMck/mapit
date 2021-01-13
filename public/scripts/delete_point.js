ed1d02cb61e842e084b1e9066b661380bead02f9
Stewart <stewart.mckinlay@gmail.com>
2021-01-12 23:09:28 -0700

    Implement delete point functionality with DB support

--- public/scripts/delete_point.js
+++ public/scripts/delete_point.js
@@ -1,11 +1,24 @@
-/*global $, google, document, window*/
+/*global $, window*/
 
 $(() => {
   const deletePoint = function (pointNumber) {
+    const point = window.points[pointNumber];
     console.log("deleting point:", pointNumber);
 
-    window.points[pointNumber].setMap(null);
+    point.setMap(null);
     delete window.points[pointNumber];
+    if (point.dbPoint) {
+      $.ajax({
+        method: "POST",
+        url: `/api/points/${point.dbPoint.id}/delete`
+      })
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
   };
 
   window.deletePoint = deletePoint;
