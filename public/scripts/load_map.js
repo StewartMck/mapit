a0b33d3521cf5ccc40ad7098b425c7fbb2400a83
Stewart <stewart.mckinlay@gmail.com>
2021-01-14 17:08:47 -0700

    Implement search functionality

--- public/scripts/load_map.js
+++ public/scripts/load_map.js
@@ -20,6 +20,11 @@ $(() => {
       document.getElementById(htmlElement),
       options
     );
+
+    const input = document.getElementById("pac-input");
+
+    autoComplete(input, map);
+
     // creating a new Point (empty {} is passed in because there is no db entry)
     map.addListener("click", (event) => {
       window.addPoint({}, event.latLng, map);
@@ -29,7 +34,7 @@ $(() => {
     // event listeners for point info window
     google.maps.event.addListener(window.infoWindow, "domready", () => {
       const deletePoint = document.getElementById("delete_marker");
-      deletePoint.onclick = (event) => {
+      deletePoint.onclick = () => {
         window.deletePoint(parseInt(deletePoint.getAttribute("data-id")));
       };
       const savePoint = document.getElementById("save_marker");
@@ -100,15 +105,40 @@ $(() => {
       });
   };
 
-  const createSearchBar = function (map) {
-    // console.log("input:", $("#pac-input"));
-    // const input = $("#pac-input");
-    // const searchBox = new google.maps.places.SearchBox(input);
-    // console.log(map);
-    // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-    // map.addListener("places-changed", () => {
-    //   const places = searchBox.getPlaces();
-    // });
+  const autoComplete = function (input, map) {
+    const autocomplete = new google.maps.places.Autocomplete(input);
+    // Bind the map's bounds (viewport) property to the autocomplete object,
+    // so that the autocomplete requests use the current map bounds for the
+    // bounds option in the request.
+    autocomplete.bindTo("bounds", map);
+    // Set the data fields to return when the user selects a place.
+    autocomplete.setFields(["address_components", "geometry", "icon", "name"]);
+
+    const marker = new google.maps.Marker({
+      map,
+      anchorPoint: new google.maps.Point(0, -29),
+    });
+    autocomplete.addListener("place_changed", () => {
+      marker.setVisible(false);
+      $(input).val("");
+      const place = autocomplete.getPlace();
+      if (!place.geometry) {
+        // User entered the name of a Place that was not suggested and
+        // pressed the Enter key, or the Place Details request failed.
+        window.alert("No details available for input: '" + place.name + "'");
+        return;
+      }
+
+      // If the place has a geometry, then present it on a map.
+      if (place.geometry.viewport) {
+        map.fitBounds(place.geometry.viewport);
+      } else {
+        map.setCenter(place.geometry.location);
+        map.setZoom(17); // Why 17? Because it looks good.
+      }
+      marker.setPosition(place.geometry.location);
+      marker.setVisible(true);
+    });
   };
 
   window.getMap = getMap;
